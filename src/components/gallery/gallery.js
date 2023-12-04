7ce3a969c1990197e96a580097cd79e42b65d42d
Virginia <lavirgin@gmail.com>
2023-12-04 19:18:27 +0100

    fix gallery columns - end of day commit

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -3,15 +3,22 @@ import './gallery.css';
 
 export const populateGallery = (gallery, listOfImages) => {
   gallery.innerHTML = '';
-  const div = document.createElement('div');
-  div.classList.add('column');
-  for (let i = 0; i < listOfImages.length / 2; i++) {
-    createCard(div, listOfImages[i]);
+  for (const image of listOfImages) {
+    createCard(gallery, image);
   }
-  const div2 = document.createElement('div');
-  div2.classList.add('column');
-  for (let i = listOfImages.length / 2; i < listOfImages.length; i++) {
-    createCard(div2, listOfImages[i]);
-  }
-  gallery.append(div, div2);
 };
+
+// export const populateGallery = (gallery, listOfImages) => {
+//   gallery.innerHTML = '';
+//   const div = document.createElement('div');
+//   div.classList.add('column');
+//   for (let i = 0; i < listOfImages.length / 2; i++) {
+//     createCard(div, listOfImages[i]);
+//   }
+//   const div2 = document.createElement('div');
+//   div2.classList.add('column');
+//   for (let i = listOfImages.length / 2; i < listOfImages.length; i++) {
+//     createCard(div2, listOfImages[i]);
+//   }
+//   gallery.append(div, div2);
+// };
